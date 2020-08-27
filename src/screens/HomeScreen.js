c20e55c3f8230a56917611ff8f2f2be127557577
Andrew-R-Lawler <61293731+Andrew-R-Lawler@users.noreply.github.com>
2020-08-27 17:56:45 -0500

    added button to Home Screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,14 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button } from "react-native";
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>New Content</Text>;
+  return <View>
+    <Text style={styles.text}>New Content</Text>
+    <Button
+      title="Go to Components Demo"
+      onPress={() => console.log('Button pressed')}
+    />
+  </View>;
 };
 
 const styles = StyleSheet.create({
